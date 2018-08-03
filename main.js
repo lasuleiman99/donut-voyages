36019f212a63724dc63ec5b36540a12f13e899c0
lasuleiman99 <40044936+lasuleiman99@users.noreply.github.com>
2018-08-02 18:06:44 -0700

    j
    k

--- main.js
+++ main.js
@@ -1,11 +1,20 @@
 // this is a link to my airtable 
 var airtable_list_url = https://api.airtable.com/v0/appTHQ46vsqL0aC7y/Table%201?api_key=keyxun8ekWUao1b2N
+var cardTemplate = function (location,city,address,name of location,picture,links,shop link) {
+  return ``
+}
+
+
 
 $.getJSON( airtable_list_url, function( data ) {
-    +  var items = [];
-    +  $.each( data.records, function( key, val ) {
-    +    console.log(val.fields)
-    +    items.push(`<h2>${val.fields['Name']}</h2>`);
-    +  });
-    +  $(".list-view").append(items.join(''));
-    +});
+    var items = [];
+    $.each( data.records, function( key, val ) {
+      // console.log(val.fields)
+      var name = val.fields['Name'];
+      var address = val.fields['Address'];
+      var picture = val.fields['Pictures'][0] ? val.fields['Pictures'][0].url : null;
+     // var html = cardTemplate(name, address, picture);
+      items.push(html);
+    });
+    $(".list-view").append(items.join(''));
+  });
